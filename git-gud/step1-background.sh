8804bda7bf05c6ada0f3ffaadfe5ea08a443c7ab
Wezley Singleton <wezley.singleton@gmail.com>
2022-03-28 18:49:56 -0400

    edit formatting (trying to get rid of blank lines after scripts run

--- git-gud/step1-background.sh
+++ git-gud/step1-background.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
-
 rm /root/project -R
 rm /root/target -R
-
 echo "done" >> /root/katacoda-finished
-
 unzip study-cards.zip
 rm -R study-cards.zip
-
 echo "done" >> /root/katacoda-background-finished
