da8dc68c8a9063026bdb51a13e3617782e6c6102
Wezley Singleton <wezley.singleton@gmail.com>
2022-03-29 13:37:47 -0400

    working on silencing debconf warning output

--- linux-playground/assets/env-setup.sh
+++ linux-playground/assets/env-setup.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 echo "done" >> /tmp/katacoda-finished
 sudo apt-get update > /dev/null
+sudo apt-get install apt-utils -y --no-install-recommends > /dev/null
 echo "Installing Git..."
 sudo apt-get install git -y > /dev/null
 echo "Git installed."
+
 echo "Installing Java 8 JDK..."
 sudo apt-get install openjdk-8-jdk -y > /dev/null
 echo "Java 8 JDK installed."
+
 echo "Installing Apache Maven..."
 sudo apt-get install maven -y > /dev/null
 echo "Maven installed."
+
 echo "Installing Apache Tomcat..."
 sudo wget "https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.60/bin/apache-tomcat-9.0.60.zip" -q -O tomcat.zip > /dev/null
 unzip tomcat.zip > /dev/null
@@ -17,4 +21,6 @@ mv apache-tomcat-9.0.60/ /usr/local/
 rm tomcat.zip
 chmod 570 /usr/local/apache-tomcat-9.0.60/ -R
 echo "Apache Tomcat installed."
+
+sleep 2
 echo "done" >> /tmp/background-finished
