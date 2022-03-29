ee35c623892458d928e411be920f51111f7d118b
Wezley Singleton <wezley.singleton@gmail.com>
2022-03-29 13:33:40 -0400

    removing silencer pipe to debug unzip issue

--- git-gud/step1-background.sh
+++ git-gud/step1-background.sh
@@ -2,6 +2,6 @@
 echo "done" >> /tmp/katacoda-finished
 rm /root/project -R > /dev/null &
 rm /root/target -R > /dev/null &
-unzip study-cards.zip > /dev/null &
+unzip study-cards.zip
 rm -R study-cards.zip > /dev/null &
 echo "done" >> /tmp/background-finished
--- git-gud/step1-setup.sh
+++ git-gud/step1-setup.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sleep 1; wait.sh
+sleep 1; wait.sh && clear
