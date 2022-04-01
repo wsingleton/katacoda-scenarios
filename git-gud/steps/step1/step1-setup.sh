e8afe69b29ffd0a1417c49a7ec552209388bbdae
Wezley Singleton <wezley.singleton@gmail.com>
2022-04-01 14:59:51 -0400

    grabbing scenario submission template from s3 bucket

--- git-gud/steps/step1/step1-setup.sh
+++ git-gud/steps/step1/step1-setup.sh
@@ -5,4 +5,5 @@ rm /root/project/ -R
 rm /root/target/ -R 
 unzip study-cards.zip >> /dev/null &
 rm study-cards.zip
+wget -O https://katakoda-scenario-assets.s3.amazonaws.com/scenario-submission-details.json
 echo "done" >> /tmp/load-step-complete
