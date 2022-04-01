0608ad05fe90688b09a7bcafd072492d52d4f7e6
Wezley Singleton <wezley.singleton@gmail.com>
2022-04-01 14:50:12 -0400

    step1 setup

--- git-gud/steps/step1/step1-setup.sh
+++ git-gud/steps/step1/step1-setup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 echo "TODO: step setup implementation"
+rm /root/project/ -R
+rm /root/target/ -R 
+unzip study-cards.zip >> /dev/null &
+rm study-cards.zip
 sleep 5;
 echo "done" >> /tmp/load-step-complete
