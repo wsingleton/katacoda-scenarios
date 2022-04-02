a3e432c3a366e0519a6123d0f50e51cf7fc87ce8
Wezley Singleton <wezley.singleton@gmail.com>
2022-04-02 17:56:21 -0400

    trying to get rid of blank lines left over after script runs

--- git-gud/steps/step1/step1-setup.sh
+++ git-gud/steps/step1/step1-setup.sh
@@ -4,8 +4,8 @@ echo "TODO: step setup implementation"
 rm /root/project/ -R
 rm /root/target/ -R 
 wget "https://katakoda-scenario-assets.s3.amazonaws.com/study-cards.zip" -q -O study-cards.zip
-unzip study-cards.zip >> /dev/null
-rm study-cards.zip
+unzip study-cards.zip > /dev/null 2>&1
+rm study-cards.zip > /dev/null 2>&1
 wget "https://katakoda-scenario-assets.s3.amazonaws.com/who-dis.sh" -q -O who-dis.sh
-chmod 775 who-dis.sh
+chmod 775 who-dis.sh > /dev/null 2>&1
 clear
