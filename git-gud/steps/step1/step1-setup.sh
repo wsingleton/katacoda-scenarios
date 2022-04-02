668f78f8ecaffc110dfe8e677838fffcbd8017ac
Wezley Singleton <wezley.singleton@gmail.com>
2022-04-02 19:15:03 -0400

    still working on getting bash to be quieter

--- git-gud/steps/step1/step1-setup.sh
+++ git-gud/steps/step1/step1-setup.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-set +o verbose
-rm /root/project/ -R
-rm /root/target/ -R 
-wget "https://katakoda-scenario-assets.s3.amazonaws.com/study-cards.zip" -q -O /root/study-cards.zip
+rm /root/project/ -R &
+rm /root/target/ -R &
+wget "https://katakoda-scenario-assets.s3.amazonaws.com/study-cards.zip" -q -O /root/study-cards.zip &
 unzip /root/study-cards.zip > /dev/null 2>&1
-rm /root/study-cards.zip
-wget "https://katakoda-scenario-assets.s3.amazonaws.com/learner-details.sh" -q -O /root/learner-details.sh
-chmod 775 /root/learner-details.sh
+rm /root/study-cards.zip &
+(wget "https://katakoda-scenario-assets.s3.amazonaws.com/learner-details.sh" -q -O /root/learner-details.sh && chmod 775 /root/learner-details.sh) &
 clear
