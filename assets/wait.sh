4e0864dd6891a6ea5dee5b2a27f54d12d921d0d4
Wezley Singleton <wezley.singleton@gmail.com>
2022-04-02 19:31:52 -0400

    reintegrating wait spinner

---
+++ assets/wait.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+show_progress()
+{
+  echo -n "Starting scenario"
+  local -r pid="${1}"
+  local -r delay='0.75'
+  local spinstr='\|/-'
+  local temp
+  while true; do 
+    sudo grep -i "done" /root/katacoda-finished &> /dev/null
+    if [[ "$?" -ne 0 ]]; then     
+      temp="${spinstr#?}"
+      printf " [%c]  " "${spinstr}"
+      spinstr=${temp}${spinstr%"${temp}"}
+      sleep "${delay}"
+      printf "\b\b\b\b\b\b"
+    else
+      break
+    fi
+  done
+  printf "    \b\b\b\b"
+  echo ""
+  echo "Scenario started"
+  echo -n "Configuring scenario"
+  while true; do 
+    sudo grep -i "done" /root/katacoda-background-finished &> /dev/null
+    if [[ "$?" -ne 0 ]]; then     
+      temp="${spinstr#?}"
+      printf " [%c]  " "${spinstr}"
+      spinstr=${temp}${spinstr%"${temp}"}
+      sleep "${delay}"
+      printf "\b\b\b\b\b\b"
+    else
+      break
+    fi
+  done
+  printf "    \b\b\b\b"
+  echo ""
+  echo "Scenario configured"
+}
+
+show_progress
+
+# Attribution: https://github.com/katacoda/scenario-examples/tree/master/displaying-progress-spinner/assets
---
+++ git-gud/steps/step1/step1-background.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "done" >> /root/katacoda-finished
+rm /root/project/ -R
+rm /root/target/ -R
+wget "https://katakoda-scenario-assets.s3.amazonaws.com/study-cards.zip" -q -O /root/study-cards.zip &
+unzip /root/study-cards.zip > /dev/null 2>&1
+rm /root/study-cards.zip
+wget "https://katakoda-scenario-assets.s3.amazonaws.com/learner-details.sh" -q -O /root/learner-details.sh 
+chmod 775 /root/learner-details.sh
+echo "done" >> /root/katacoda-background-finished
