754a9cada94267bfa2e34f735cb2b058a17c66a6
Wezley Singleton <wezley.singleton@gmail.com>
2022-04-02 18:53:12 -0400

    another attempt to silence bash noise during setup

--- git-gud/steps/step1/step1-setup.sh
+++ git-gud/steps/step1/step1-setup.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-{
-    rm /root/project/ -R
-    rm /root/target/ -R 
-    wget "https://katakoda-scenario-assets.s3.amazonaws.com/study-cards.zip" -q -O study-cards.zip
-    unzip study-cards.zip
-    rm study-cards.zip
-    wget "https://katakoda-scenario-assets.s3.amazonaws.com/who-dis.sh" -q -O who-dis.sh
-    chmod 775 who-dis.sh 
-} > /dev/null 2>&1
+set +o verbose
+rm /root/project/ -R
+rm /root/target/ -R 
+wget "https://katakoda-scenario-assets.s3.amazonaws.com/study-cards.zip" -q -O /root/study-cards.zip
+unzip /root/study-cards.zip > /dev/null 2>&1
+rm /root/study-cards.zip
+wget "https://katakoda-scenario-assets.s3.amazonaws.com/who-dis.sh" -q -O /root/who-dis.sh
+chmod 775 /root/who-dis.sh
 clear
--- git-gud/steps/step9/submit-scenario.sh
+++ git-gud/steps/step9/submit-scenario.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-echo "Submitting scenario..."
+set +o verbose
+
+if [ -e /root/submission.json ]; 
+then
 curl \
     -X POST \
     -H "Content-Type: application/json" \
     -d @/root/submission.json \
-    http://harvester-env.eba-9zpjj7px.us-east-1.elasticbeanstalk.com/harvester/katacoda-scenario/submissions
-
+    http://harvester-env.eba-9zpjj7px.us-east-1.elasticbeanstalk.com/harvester/katacoda-scenario/submissions \
+>> /root/response.json
+else
+    echo "No scenario data to submit."
+fi
+clear
