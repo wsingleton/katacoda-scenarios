ce2f02bcde605a2ae54ebfc0a304ccd1cd771ed0
Wezley Singleton <wezley.singleton@gmail.com>
2022-04-02 13:04:38 -0400

    testing scenario submission to deployed harvester api

--- git-gud/steps/step9/submit-scenario.sh
+++ git-gud/steps/step9/submit-scenario.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "Submitting scenario..."
-curl -X POST -H "Content-Type: application/json" -d @/root/scenario-submission-details.json
+curl -X POST -H "Content-Type: application/json" -d @/root/scenario-submission-details.json https://harvester-env.eba-9zpjj7px.us-east-1.elasticbeanstalk.com/harvester/submissions
 
