b09c44db9f0b9b93480260be3735d539da3e4e98
Wezley Singleton <wezley.singleton@gmail.com>
2022-04-02 17:15:22 -0400

    fix submission file name in submission step

--- git-gud/steps/step9/submit-scenario.sh
+++ git-gud/steps/step9/submit-scenario.sh
@@ -4,6 +4,6 @@ echo "Submitting scenario..."
 curl \
     -X POST \
     -H "Content-Type: application/json" \
-    -d @/root/scenario-submission-details.json \
+    -d @/root/submission.json \
     http://harvester-env.eba-9zpjj7px.us-east-1.elasticbeanstalk.com/harvester/katacoda-scenario/submissions
 
