35dbc28dbccaebd236665b7749933ae073e33230
Wezley Singleton <wezley.singleton@gmail.com>
2022-04-02 17:14:47 -0400

    added logic to gather learner info from console instead of expecting user to fill out json file themselves

--- git-gud/steps/step1/step1-setup.sh
+++ git-gud/steps/step1/step1-setup.sh
@@ -6,6 +6,22 @@ rm /root/target/ -R
 wget "https://katakoda-scenario-assets.s3.amazonaws.com/study-cards.zip" -q -O study-cards.zip
 unzip study-cards.zip >> /dev/null
 rm study-cards.zip
-wget "https://katakoda-scenario-assets.s3.amazonaws.com/scenario-submission-details.json" -q -O scenario-submission-details.json
 clear
-clear
+echo 'Would you like to submit your completion of this scenario? [y/n] '
+read answer
+if [["${answer,,}" == "y"]] || [["${answer,,}" == "yes"]];
+then
+    read -p "First name: " firstName
+    read -p "Last name: " lastName
+    read -p "Email address: " email
+    submissionJson="{
+        \"scenarioId\":\"3acb5508-d71a-439c-8e82-14ccacddff1b\",
+        \"submitter\":{
+            \"firstName\":\"${firstName}\",
+            \"lastName\":\"${lastName}\",
+            \"email\":\"${email}\"
+        }
+    }"
+    printf "$submissionJson" >> /root/submission.json
+fi
+echo "When you are ready, please continue to the next step."
