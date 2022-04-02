98e47c7ec0d8878fe2d7b2f94c0ff6641b69cd61
Wezley Singleton <wezley.singleton@gmail.com>
2022-04-02 13:17:56 -0400

    fix bad api url

--- git-gud/steps/step9/submit-scenario.sh
+++ git-gud/steps/step9/submit-scenario.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "Submitting scenario..."
-curl -X POST -H "Content-Type: application/json" -d @/root/scenario-submission-details.json http://harvester-env.eba-9zpjj7px.us-east-1.elasticbeanstalk.com/harvester/submissions
+curl -X POST -H "Content-Type: application/json" -d @/root/scenario-submission-details.json http://harvester-env.eba-9zpjj7px.us-east-1.elasticbeanstalk.com/harvester/katacoda-scenario/submissions
 
