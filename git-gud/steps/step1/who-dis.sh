8c7f3e854734cc2d2d22aa11f4b981dc213aadfb
Wezley Singleton <wezley.singleton@gmail.com>
2022-04-02 17:51:12 -0400

    test to see if who-dis can run correctly as a foreground script

---
+++ git-gud/steps/step1/who-dis.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+read -p "Would you like for your information to be submitted when you complete this scenario? [y/n] " answer
+if [ "$answer" = "y" ] || [ "$answer" = "Y" ] || [ "$answer" = "yes" ] || [ "$answer" = "YES" ];
+then
+    read -p "First name: " firstName
+    read -p "Last name: " lastName
+    read -p "Email address: " email
+    submissionJson="{
+        \"scenarioId\":\"3acb5508-d71a-439c-8e82-14ccacddff1b\",
+        \"submitter\":{
+            \"firstName\":\"${firstName}\",
+            \"lastName\":\"${lastName}\",
+            \"email\":\"${email}\"
+        }
+    }"
+    printf "$submissionJson" >> /root/submission.json
+fi
+echo "When you are ready, please continue to the next step."
+
