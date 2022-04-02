374b9322679d810f9f6e87e998cf00f7cd84c3ba
Wezley Singleton <wezley.singleton@gmail.com>
2022-04-01 20:38:57 -0400

    adding silencer pipe back to unzip

--- git-gud/steps/step1/step1-setup.sh
+++ git-gud/steps/step1/step1-setup.sh
@@ -4,6 +4,6 @@ echo "TODO: step setup implementation"
 rm /root/project/ -R
 rm /root/target/ -R 
 wget "https://katakoda-scenario-assets.s3.amazonaws.com/study-cards.zip" -q -O study-cards.zip
-unzip study-cards.zip
+unzip study-cards.zip >> /dev/null
 rm study-cards.zip
 wget "https://katakoda-scenario-assets.s3.amazonaws.com/scenario-submission-details.json" -q -O scenario-submission-details.json
