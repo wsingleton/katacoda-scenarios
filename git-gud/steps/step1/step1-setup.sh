ae0fb7c31880fde0cf99f7ee36acc424ec567ebc
Wezley Singleton <wezley.singleton@gmail.com>
2022-04-01 21:41:14 -0400

    adding extra clear (test)

--- git-gud/steps/step1/step1-setup.sh
+++ git-gud/steps/step1/step1-setup.sh
@@ -8,3 +8,4 @@ unzip study-cards.zip >> /dev/null
 rm study-cards.zip
 wget "https://katakoda-scenario-assets.s3.amazonaws.com/scenario-submission-details.json" -q -O scenario-submission-details.json
 clear
+clear
