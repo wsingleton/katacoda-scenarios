de58a28e40c9a2b40458bd6b98c240a72dac6a74
Wezley Singleton <wezley.singleton@gmail.com>
2022-04-02 17:58:55 -0400

    silencer experiment

--- git-gud/steps/step1/step1-setup.sh
+++ git-gud/steps/step1/step1-setup.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-echo "TODO: step setup implementation"
-rm /root/project/ -R
-rm /root/target/ -R 
-wget "https://katakoda-scenario-assets.s3.amazonaws.com/study-cards.zip" -q -O study-cards.zip
-unzip study-cards.zip > /dev/null 2>&1
-rm study-cards.zip > /dev/null 2>&1
-wget "https://katakoda-scenario-assets.s3.amazonaws.com/who-dis.sh" -q -O who-dis.sh
-chmod 775 who-dis.sh > /dev/null 2>&1
-clear
+{
+    echo "TODO: step setup implementation"
+    rm /root/project/ -R
+    rm /root/target/ -R 
+    wget "https://katakoda-scenario-assets.s3.amazonaws.com/study-cards.zip" -q -O study-cards.zip
+    unzip study-cards.zip
+    rm study-cards.zip
+    wget "https://katakoda-scenario-assets.s3.amazonaws.com/who-dis.sh" -q -O who-dis.sh
+    chmod 775 who-dis.sh 
+    clear
+} > /dev/null 2>&1
