aad3e1aa8ca03a120e9a64641a2e5d422099c67c
Wezley Singleton <wezley.singleton@gmail.com>
2022-04-02 19:08:11 -0400

    submit-scenario now prints a completion msg

--- git-gud/steps/step9/submit-scenario.sh
+++ git-gud/steps/step9/submit-scenario.sh
@@ -4,13 +4,14 @@ set +o verbose
 
 if [ -e /root/submission.json ]; 
 then
-curl \
-    -X POST \
-    -H "Content-Type: application/json" \
-    -d @/root/submission.json \
-    http://harvester-env.eba-9zpjj7px.us-east-1.elasticbeanstalk.com/harvester/katacoda-scenario/submissions \
->> /root/response.json
+    curl \
+        -X POST \
+        -H "Content-Type: application/json" \
+        -d @/root/submission.json \
+        http://harvester-env.eba-9zpjj7px.us-east-1.elasticbeanstalk.com/harvester/katacoda-scenario/submissions \
+    >> /root/response.json
+    clear
+    echo "Scenario details submitted. Please continue."
 else
-    echo "No scenario data to submit."
+    echo "No scenario data to submit. Please continue."
 fi
-clear
