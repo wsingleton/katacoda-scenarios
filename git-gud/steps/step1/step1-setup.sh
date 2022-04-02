624c7f0705933061a3ea8c598e346ad714d7c207
Wezley Singleton <wezley.singleton@gmail.com>
2022-04-02 19:19:49 -0400

    improving step 1 terminal output during env setup period

--- git-gud/steps/step1/step1-setup.sh
+++ git-gud/steps/step1/step1-setup.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-rm /root/project/ -R &
-rm /root/target/ -R &
+rm /root/project/ -R
+rm /root/target/ -R
 wget "https://katakoda-scenario-assets.s3.amazonaws.com/study-cards.zip" -q -O /root/study-cards.zip &
 unzip /root/study-cards.zip > /dev/null 2>&1
-rm /root/study-cards.zip &
+rm /root/study-cards.zip
 (wget "https://katakoda-scenario-assets.s3.amazonaws.com/learner-details.sh" -q -O /root/learner-details.sh && chmod 775 /root/learner-details.sh) &
 clear
+echo 'Setting up your environment. Please wait...'
+sleep 5 && echo "Environment setup complete" && sleep 1 && clear
