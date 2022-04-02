584f72a5522b7ce5058afd9b8e8482aab69e33d9
Wezley Singleton <wezley.singleton@gmail.com>
2022-04-02 17:53:25 -0400

    revert to fetch who-dis from asset bucket

--- git-gud/steps/step1/step1-setup.sh
+++ git-gud/steps/step1/step1-setup.sh
@@ -6,4 +6,6 @@ rm /root/target/ -R
 wget "https://katakoda-scenario-assets.s3.amazonaws.com/study-cards.zip" -q -O study-cards.zip
 unzip study-cards.zip >> /dev/null
 rm study-cards.zip
+wget "https://katakoda-scenario-assets.s3.amazonaws.com/who-dis.sh" -q -O who-dis.sh
+chmod 775 who-dis.sh
 clear
