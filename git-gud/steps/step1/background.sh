004becc108ccadb69040ca27a909913f1f0bc8ea
Wezley Singleton <wezley.singleton@gmail.com>
2022-04-04 22:41:25 -0400

    adding log-scenario (formerly learner-details) as asset instead of fetching from asset bucket

--- git-gud/steps/step1/background.sh
+++ git-gud/steps/step1/background.sh
@@ -5,7 +5,5 @@ rm /root/target/ -R
 wget "https://katakoda-scenario-assets.s3.amazonaws.com/study-cards.zip" -q -O /root/study-cards.zip &
 unzip /root/study-cards.zip > /dev/null 2>&1
 rm /root/study-cards.zip
-wget "https://katakoda-scenario-assets.s3.amazonaws.com/learner-details.sh" -q -O /root/learner-details.sh 
-chmod 775 /root/learner-details.sh
 sleep 3
 echo "done" >> /tmp/katacoda-background-finished
