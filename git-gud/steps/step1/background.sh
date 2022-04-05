2770e2ccaa4f5b7c67617086c63bfadbb305a390
Wezley Singleton <wezley.singleton@gmail.com>
2022-04-05 15:32:47 -0400

    added missing text attrs to steps in index.json

--- git-gud/steps/step1/background.sh
+++ git-gud/steps/step1/background.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 echo "done" >> /tmp/katacoda-finished
-rm /root/project/ -R
-rm /root/target/ -R
-wget "https://katakoda-scenario-assets.s3.amazonaws.com/study-cards.zip" -q -O /root/study-cards.zip &
-unzip /root/study-cards.zip > /dev/null 2>&1
-rm /root/study-cards.zip
-sleep 3
+(
+    rm /root/project/ -R
+    rm /root/target/ -R
+    wget "https://katakoda-scenario-assets.s3.amazonaws.com/study-cards.zip" -q -O /root/study-cards.zip &
+    unzip /root/study-cards.zip > /dev/null 2>&1
+    rm /root/study-cards.zip
+    sleep 3
+) 2> /tmp/scenario-error.log
 echo "done" >> /tmp/katacoda-background-finished
